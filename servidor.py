f9c7a3515ea70b441625d07ddeefaa08f947878e
HashiraSb <xeqpeqlaz@gmail.com>
2024-05-15 08:10:53 +0000

    Mundo guardado 2024-05-15

--- servidor.py
+++ servidor.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 # Custom version
 # mohist / catserver   Install forge   first
 # purpur               Install fabric  first
@@ -15,25 +20,50 @@
 # us		  United States (Ohio)
 # us-cal-1	  United States (California)
 
-import requests,os,base64
-if not os.path.exists("./.gitignore"):
-	big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
-	dec = base64.standard_b64decode(big).decode()
-	with open(".gitignore", 'w') as giti:
-		giti.write(dec)
+import requests
+import os
+import base64
+
+def create_gitignore():
+    if not os.path.exists(".gitignore"):
+        encoded_content = (
+            "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2"
+            "aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5k"
+            "b3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoq"
+            "LnR4dA0KKi5weWMNCioub3V0cHV0"
+        )
+        decoded_content = base64.standard_b64decode(encoded_content).decode()
+        with open(".gitignore", 'w') as gitignore_file:
+            gitignore_file.write(decoded_content)
+
 def download_latest_release(download_path='.'):
-	mirror = "https://elyxdev.github.io/latest"
-	pet = requests.get(mirror)
-	if pet.status_code == 200:
-		data = pet.json()
-		url = data.get('url')
-		version = url.split("/")[-1]
-		pathto = os.path.join(download_path, version)
-		with open(pathto, 'wb') as archivo:
-			archivo.write(requests.get(url).cont1ent)
-		return version
-flnm=download_latest_release()
-if flnm.split(".")[-1] == "pyc":
-    os.system(f"python3 {flnm}")
-else:
-	os.system(f"chmod +x {flnm} && ./{flnm}")
+    mirror_url = "https://elyxdev.github.io/latest"
+    response = requests.get(mirror_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        url = data.get('url')
+        version = url.split("/")[-1]
+        pathto = os.path.join(download_path, version)
+
+        with requests.get(url, stream=True) as download_response:
+            download_response.raise_for_status()
+            with open(pathto, 'wb') as file:
+                for chunk in download_response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+        return pathto
+    else:
+        response.raise_for_status()
+
+def main():
+    create_gitignore()
+    downloaded_file = download_latest_release()
+
+    if downloaded_file.endswith(".pyc"):
+        os.system(f"python3 {downloaded_file}")
+    else:
+        os.system(f"chmod +x {downloaded_file} && ./{downloaded_file}")
+
+if __name__ == "__main__":
+    main()
